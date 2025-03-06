7045a3363cf2178dbe484049d2de2cdb0332c7cb
Konstantin Salyaev <r.cf2003@yandex.ru>
2025-03-06 13:24:01 +0300

    feat(router): add check for params in path, add docs for func

--- router/route.go
+++ router/route.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"strings"
@@ -12,23 +13,90 @@ var (
 )
 
 // Use add middleware to Router
+//
+// # Example of usage
+//
+//	func main() {
+//		r := router.NewRouter(slog.Default())
+//		r.Use(MiddlewareLogging)
+//		http.ListenAndServe("0.0.0.0:80", r)
+//	}
+//
+//	func MiddlewareLogging(ctx *router.APIContext) {
+//		ctx.Info("hello from middleware", key, data)
+//	}
 func (r *Router) Use(mw HandlerFunc) {
 	r.middleware = append(r.middleware, mw)
 }
 
 // POST handle new handlers func on path
+//
+// # Example of usage
+//
+//	type PostReq struct {
+//		Name string `json:"string"`
+//	}
+//
+//	func (p *PostReq) IsValid() bool {
+//		return len([]rune(p.Name)) > 0
+//	}
+//
+//	func main() {
+//		r := router.NewRouter(slog.Default())
+//		r.POST("path_1", MainHandler)
+//		r.POST("path_2", MainHandler, MinorHandler)
+//		http.ListenAndServe("0.0.0.0:80", r)
+//	}
+//
+//	func MainHandler(ctx *router.APIContext) {
+//		var req PostReq
+//
+//		err := ctx.Decode(&req)
+//		if err != nil {
+//			ctx.Error("failed to decode req", err)
+//			ctx.WriteFailure(http.StatusBadRequest, "invalid request")
+//			return
+//		}
+//
+//		ctx.SuccessWithData(req.Name)
+//	}
+//
+//	func MinorHandler(ctx *router.APIContext) {
+//		ctx.SuccessWithData("minor")
+//	}
 func (r *Router) POST(path string, handlers ...HandlerFunc) {
 	r.handle(http.MethodPost, path, handlers...)
 }
 
 // GET handle new handlers func on path
+//
+// # Example of usage
+//
+//	func main() {
+//		r := router.NewRouter(slog.Default())
+//		r.GET("/:usertype/:id", HandlerPath)
+//		r.GET("/user/", HandlerQuery)
+//		http.ListenAndServe("0.0.0.0:80", r)
+//	}
+//
+//	func HandlerPath(ctx *router.APIContext) {
+//		id := ctx.GetFromPath("id")
+//		userType := ctx.GetFromPath("usertype")
+//		ctx.SuccessWithData(userType + " " + id)
+//	}
+//
+//	func HandlerQuery(ctx *router.APIContext) {
+//		id := ctx.GetFromQuery("id")
+//		ctx.SuccessWithData(id)
+//	}
 func (r *Router) GET(path string, handlers ...HandlerFunc) {
 	r.handle(http.MethodGet, path, handlers...)
 }
 
 func (r *Router) handle(method, path string, handlers ...HandlerFunc) {
 	if len(handlers) == 0 {
-		panic("at least 1 handler")
+		err := fmt.Errorf("0 handlers for path: %s %s", method, path)
+		panic(err)
 	}
 
 	fullPath := r.prefix + path
@@ -36,11 +104,22 @@ func (r *Router) handle(method, path string, handlers ...HandlerFunc) {
 	parts := strings.Split(strings.Trim(fullPath, "/"), "/")
 	current := r.root
 	for _, part := range parts {
+		var isParam bool
+		var paramName string
+
+		if strings.HasPrefix(part, ":") {
+			isParam = true
+			paramName = part[1:]
+			part = "*"
+		}
+
 		if _, ok := current.children[part]; !ok {
 			current.children[part] = &node{
-				path:     part,
-				children: make(map[string]*node),
-				handler:  make(map[string][]HandlerFunc),
+				path:      part,
+				children:  make(map[string]*node),
+				handler:   make(map[string][]HandlerFunc),
+				isParam:   isParam,
+				paramName: paramName,
 			}
 		}
 		current = current.children[part]
@@ -57,21 +136,25 @@ func (r *Router) handle(method, path string, handlers ...HandlerFunc) {
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	parts := strings.Split(strings.Trim(req.URL.Path, "/"), "/")
 	current := r.root
+	params := make(map[string]string)
 
 	ctx := r.pool.Get().(*APIContext)
-	ctx.SetCtx(w, req, r.log)
+	ctx.setCtx(w, req, r.log)
 	ctx.log = ctx.log.With(slog.String("pattern", req.Method+" "+req.URL.Path))
 	ctx.w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
 	defer func() {
-		ctx.Wait()
-		ctx.Reset()
+		ctx.wait()
+		ctx.reset()
 		r.pool.Put(ctx)
 	}()
 
 	for _, part := range parts {
 		if child, ok := current.children[part]; ok {
 			current = child
+		} else if paramChild, ok := current.children["*"]; ok {
+			current = paramChild
+			params[current.paramName] = part
 		} else {
 			ctx.Error("handler not found", errors.New("handler not found"))
 			ctx.WriteFailure(http.StatusBadRequest, "bad gateway")
@@ -85,6 +168,7 @@ func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	ctx.pathParams = params
 	ctx.setHandlers(handler)
 	ctx.setMiddleware(r.middleware)
 
